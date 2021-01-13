7a1df74ddbd9658b5d41061293c0230cd9d2986a
TsuneJunya <76545359+TsuneJunya@users.noreply.github.com>
2021-01-13 16:14:11 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("m1").addEventListener("click" , ()=>{
+    document.getElementById("mo1").classList.add("active");
+    document.getElementById("modal-list").classList.add("active");
+    document.getElementById("image-list").classList.add("active");
+   
+
+})

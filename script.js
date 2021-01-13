e237b18cbaa6fd1da90816d9088b193c26593d2d
TsuneJunya <76545359+TsuneJunya@users.noreply.github.com>
2021-01-13 16:27:41 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("m1").addEventListener("click" , ()=>{
-    document.getElementById("mo1").classList.add("active");
-    document.getElementById("modal-list").classList.add("active");
-    document.getElementById("image-list").classList.add("active");
-   
-
-})

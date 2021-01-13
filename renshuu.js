e8c585c33b14b184768a6c5081f64ca240d4a5a9
TsuneJunya <76545359+TsuneJunya@users.noreply.github.com>
2021-01-13 16:17:10 +0900

    Add files via upload

---
+++ renshuu.js
@@ -0,0 +1,20 @@
+// document.getElementById("modalOpen").addEventListener("click" , ()=>{
+//     document.getElementById("modal").classList.add("active");
+//     document.getElementById("mask").classList.add("active");
+//     document.getElementById("modalClose").classList.add("active");
+// })
+
+// document.getElementById("modalClose").addEventListener("click" , ()=>{
+//     document.getElementById("modal").classList.remove("active");
+//     document.getElementById("mask").classList.remove("active");
+//     document.getElementById("modalClose").classList.remove("active");
+// })
+
+document.getElementById("m1").addEventListener("click" , ()=>{
+    document.getElementById("mo1").classList.add("active");
+    document.getElementById("modal-list").classList.add("active");
+    document.getElementById("image-list").classList.add("active");
+   
+
+})
+
